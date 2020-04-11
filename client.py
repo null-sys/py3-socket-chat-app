8220ab8464f792cdfafcad49c30e76b74c4d9e5c
Gaurav <gaurav88point@gmail.com>
2020-04-12 03:12:39 +0530

    Make chatbot

--- client.py
+++ client.py
@@ -7,14 +7,12 @@ server_port = 3000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((server_name,server_port))
-msg = input("enter a msg to server ")
 
-data = msg.encode('ascii')
-
-s.send(data,(server_name,server_port))
-
-data,server = s.recvfrom(MAX_SIZE_BYTES)
-
-recv_msg = data.decode('ascii')
-
-print('The server at {} says {!r}'.format(server,recv_msg))
+recv_msg = ""
+while recv_msg != "bye":
+	msg = input("          				")
+	data = msg.encode('ascii')
+	s.send(data)
+	data,server = s.recvfrom(MAX_SIZE_BYTES)
+	recv_msg = data.decode('ascii')
+	print(recv_msg)
