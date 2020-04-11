1ffa39380c155d8012beaf620f5b5879e61a93a2
Gaurav <gaurav88point@gmail.com>
2020-04-12 03:02:19 +0530

    use connect for more security

--- client.py
+++ client.py
@@ -2,12 +2,16 @@ import socket
 
 MAX_SIZE_BYTES = 65535
 
+server_name = "127.0.0.1"
+server_port = 3000
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((server_name,server_port))
 msg = input("enter a msg to server ")
 
 data = msg.encode('ascii')
 
-s.sendto(data,("127.0.0.1",3000))
+s.send(data,(server_name,server_port))
 
 data,server = s.recvfrom(MAX_SIZE_BYTES)
 
